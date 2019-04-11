2dceaa6058c55600d9c2e92601e2f2072f204257
kdiaz1744 <kdiaz1744@hotmail.com>
2019-04-11 16:36:04 -0400

    Added roles to register and started layout 2

--- Web_App_BioWell/Models/AccountViewModels.cs
+++ Web_App_BioWell/Models/AccountViewModels.cs
@@ -79,6 +79,8 @@ namespace Web_App_BioWell.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public string RoleName { get; set; }
     }
 
     public class ResetPasswordViewModel
